2c912a9d3317f53c348ccc45b50ce61d9ae4937b
Rael Calitro <rael06@hotmail.fr>
2022-03-15 20:33:21 +0100

    fix comments

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -1,16 +1,33 @@
+import { Launch } from "@mui/icons-material";
+import { Grid } from "@mui/material";
+import { Link } from "react-router-dom";
 import classes from "./styles.module.scss";
-import { Link } from "react-router-dom"
-import LaunchIcon from '@material-ui/icons/Launch';
-import { Grid } from "@material-ui/core";
 
-export default function Footer(){
-return (
+export default function Footer() {
+  return (
     <footer className={classes.wrapper}>
-        <Grid container direction="row" justifyContent="space-between" alignItems="center">
-            <Link to='/legal' className={classes.link}>Mentions légales</Link>
-            <Link to='/sitemap' className={classes.link}>Plan du site</Link>
-            <a href ="https://github.com/YLunch/YLunchUI" target ="_blank" className={classes.link}>©Ylunch 2021-2022<LaunchIcon fontSize="inherit"></LaunchIcon></a>
-        </Grid>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Link to="/legal" className={classes.link}>
+          Mentions légales
+        </Link>
+        <Link to="/sitemap" className={classes.link}>
+          Plan du site
+        </Link>
+        <a
+          className={classes.link}
+          href="https://github.com/YLunch/YLunchUI"
+          target="_blank"
+          rel="noreferrer"
+        >
+          ©Ylunch 2021-2022
+          <Launch fontSize="inherit" />
+        </a>
+      </Grid>
     </footer>
-)
+  );
 }
