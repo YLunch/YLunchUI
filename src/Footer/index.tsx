4818670a1ca7ebee8f20fea38337c5668192ea40
Marie Sommier <marie.a.s45@gmail.com>
2022-03-15 18:13:59 +0100

    added footer as requested

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -1,5 +1,16 @@
 import classes from "./styles.module.scss";
+import { Link } from "react-router-dom"
+import LaunchIcon from '@material-ui/icons/Launch';
+import { Grid } from "@material-ui/core";
 
-export default function Footer() {
-  return <div className={classes.wrapper}>Footer</div>;
+export default function Footer(){
+return (
+    <footer className={classes.wrapper}>
+        <Grid container direction="row" justifyContent="space-between" alignItems="center">
+            <Link to='/legal' className={classes.link}>Mentions légales</Link>
+            <Link to='/sitemap' className={classes.link}>Plan du site</Link>
+            <a href ="https://github.com/YLunch/YLunchUI" target ="_blank" className={classes.link}>©Ylunch 2021-2022<LaunchIcon fontSize="inherit"></LaunchIcon></a>
+        </Grid>
+    </footer>
+)
 }
