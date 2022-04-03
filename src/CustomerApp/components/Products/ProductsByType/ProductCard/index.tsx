2fd5e89c475409b97438a17529748918ff41cee6
Rael Calitro <rael06@hotmail.fr>
2022-04-03 18:19:21 +0200

    wip product card

--- src/CustomerApp/components/Products/ProductsByType/ProductCard/index.tsx
+++ src/CustomerApp/components/Products/ProductsByType/ProductCard/index.tsx
@@ -1,3 +1,11 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
 import { ProductReadDto } from "../../../../../models/Product";
 
 type Props = {
@@ -5,5 +13,25 @@ type Props = {
 };
 
 export default function ProductCard({ product }: Props) {
-  return <div>{product.name}</div>;
+  const { name, description, price } = product;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia component="img" alt="icone ynov" height="140" />
+      <CardContent sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography gutterBottom variant="subtitle2" component="div">
+          {name}
+          <Typography variant="body2">{description}</Typography>
+        </Typography>
+        <Typography variant="subtitle2" component="p">
+          {price}
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ float: "right" }}>
+        <Button variant="contained" size="small" sx={{ textTransform: "none" }}>
+          {" "}
+          Voir plus
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
