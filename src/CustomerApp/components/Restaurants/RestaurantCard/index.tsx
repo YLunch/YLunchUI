b2ee643339831cb6388c24cc370a4b7dd543093a
Marie-Bewiwi <marie.a.s45@gmail.com>
2022-05-06 00:11:42 +0200

    Restaurant card date logic + style

--- src/CustomerApp/components/Restaurants/RestaurantCard/index.tsx
+++ src/CustomerApp/components/Restaurants/RestaurantCard/index.tsx
@@ -1,11 +1,15 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography} from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { RestaurantReadDto } from "../../../../common/models/Restaurant";
+import { RestaurantReadDto,OpeningTimeReadDto} from "../../../../common/models/Restaurant";
+import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import QueryBuilderIcon from '@mui/icons-material/QueryBuilder';
+import OpeningTimes from "../OpeningTimes";
 import classes from "./styles.module.scss";
 
 type Props = {
-  restaurant: RestaurantReadDto;
+  restaurant: RestaurantReadDto
 };
+
 export default function RestaurantCard({ restaurant }: Props) {
   const navigate = useNavigate();
   function handleClick() {
@@ -14,7 +18,17 @@ export default function RestaurantCard({ restaurant }: Props) {
 
   return (
     <Box className={classes.wrapper} onClick={handleClick}>
-      <Typography variant="h1">{restaurant.name}</Typography>
+      <Typography variant="h1" sx={{
+        fontSize : "clamp(2rem, 5vw, 4rem)",
+        margin : "2%"
+      }}>{restaurant.name}</Typography>
+      <div className={classes.openingswrapper}>
+      <div className={classes.openings}>
+        <p className={classes.hours}>Horaires  </p>
+        <QueryBuilderIcon></QueryBuilderIcon> Restauration<OpeningTimes openingTimes={restaurant.placeOpeningTimes} />
+        <HourglassBottomIcon></HourglassBottomIcon> Commandez<OpeningTimes openingTimes={restaurant.orderOpeningTimes} />
+      </div>
+      </div>
     </Box>
   );
 }
